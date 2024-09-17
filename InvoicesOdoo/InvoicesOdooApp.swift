a266a3aa0177eec5e3bfb6ee0c90deef1d66ffe7
EvgenyKreger <evgeny_kreger@mail.ru>
2024-09-17 11:20:46 +0300

    Initial Commit

---
+++ InvoicesOdoo/InvoicesOdooApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoicesOdooApp.swift
+//  InvoicesOdoo
+//
+//  Created by taqtile on 17.09.24.
+//
+
+import SwiftUI
+
+@main
+struct InvoicesOdooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
